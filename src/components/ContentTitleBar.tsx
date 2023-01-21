08f2eadb9e0dea1e09e31e9b7b1283b2c074be3b
buzzind99 <buzzind99@gmail.com>
2023-01-21 14:36:39 +0700

    added ContentTitleBar component

---
+++ src/components/ContentTitleBar.tsx
@@ -0,0 +1,12 @@
+const ContentTitleBar: React.FC = () => {
+  return (
+    <h1
+      id="content-title"
+      className="w-[66.25rem] mx-auto bg-[#e1e7f5] border-b-[1px] border-[#2e51a2] font-[Verdana] font-bold flex items-center pt-[0.25rem] pb-[0.125rem] px-[0.625rem]"
+    >
+      Welcome to MyAnimeList.net!
+    </h1>
+  );
+};
+
+export default ContentTitleBar;
