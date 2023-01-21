4cdc571fad0a490e372bbf34b764f853027c75eb
buzzind99 <buzzind99@gmail.com>
2023-01-21 14:37:22 +0700

    added ContentTitleBar component

--- src/components/ContentTitleBar.tsx
+++ src/components/ContentTitleBar.tsx
@@ -1,10 +1,14 @@
-const ContentTitleBar: React.FC = () => {
+type Props = {
+  children?: React.ReactNode;
+};
+
+const ContentTitleBar: React.FC<Props> = ({ children }) => {
   return (
     <h1
       id="content-title"
       className="w-[66.25rem] mx-auto bg-[#e1e7f5] border-b-[1px] border-[#2e51a2] font-[Verdana] font-bold flex items-center pt-[0.25rem] pb-[0.125rem] px-[0.625rem]"
     >
-      Welcome to MyAnimeList.net!
+      {children}
     </h1>
   );
 };
