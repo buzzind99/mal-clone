60d122d270627adb5a95f72c0377f6b8b9bf290f
buzzind99 <buzzind99@gmail.com>
2023-01-21 14:37:50 +0700

    added MainContainer component

---
+++ src/components/MainContainer.tsx
@@ -0,0 +1,9 @@
+type Props = {
+    children?: React.ReactNode;
+}
+
+const MainContainer: React.FC<Props> = ({children}) => {
+  return <div className="w-[66.25rem] mx-auto border-x-[1px] border-b-[1px] border-[#e5e5e5]">{children}</div>;
+};
+
+export default MainContainer;
