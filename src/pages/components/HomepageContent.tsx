75830de96810028eb9e8505fa495376385d436a0
buzzind99 <buzzind99@gmail.com>
2023-01-22 12:19:16 +0700

    added components to homepage

--- src/pages/components/HomepageContent.tsx
+++ src/pages/components/HomepageContent.tsx
@@ -1,15 +1,30 @@
 import MALxJPN from "./MALxJPN";
+import MostPopularAnimePlaceholder from "./MostPopularAnimePlaceholder";
+import TopAiringAnimePlaceholder from "./TopAiringAnimePlaceholder";
+import TopUpcomingAnimePlaceholder from "./TopUpcomingAnimePlaceholder copy";
 
 const HomepageContent: React.FC = () => {
   return (
-    <div className="bg-white relative text-left h-screen flex py-[0.625rem]">
+    <div className="bg-white relative text-left flex py-[0.625rem]">
       <div
         id="content-left"
         className="inline-block w-[46.5395rem] border-r-[1px] border-[#e5e5e5]"
       >
-        <MALxJPN />
+        <div className="mx-[0.625rem] mb-[1.25rem]">
+          <MALxJPN />
+        </div>
       </div>
-      <div id="content-right" className="inline-block w-[20rem]"></div>
+      <div id="content-right" className="inline-block w-[20rem]">
+        <div className="mx-[0.5rem] mt-1">
+          <TopAiringAnimePlaceholder />
+        </div>
+        <div className="mx-[0.5rem] mt-[1.25rem]">
+          <TopUpcomingAnimePlaceholder />
+        </div>
+        <div className="mx-[0.5rem] mt-[1.25rem]">
+          <MostPopularAnimePlaceholder />
+        </div>
+      </div>
     </div>
   );
 };
