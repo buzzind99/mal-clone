6af6e85ff7dae4777afce3e8b4c5529fd2142243
buzzind99 <buzzind99@gmail.com>
2023-01-22 12:17:44 +0700

    changed styling

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -3,7 +3,7 @@ type Props = {
 }
 
 const MainContainer: React.FC<Props> = ({children}) => {
-  return <div className="w-[66.25rem] mx-auto border-x-[1px] border-b-[1px] border-[#e5e5e5]">{children}</div>;
+  return <div className="w-[66.25rem] mx-auto border-x-[1px] border-b-[1px] border-[#e5e5e5] mb-[3.375rem]">{children}</div>;
 };
 
 export default MainContainer;
