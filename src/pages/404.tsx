ef49af48e962f7649ce337f65bb371a42fc38703
buzzind99 <buzzind99@gmail.com>
2023-01-22 21:21:49 +0700

    added 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,51 @@
+import ContentTitleBar from "@/components/ContentTitleBar";
+import FooterChartPlaceholder from "@/components/FooterChartPlaceholder";
+import MainContainer from "@/components/MainContainer";
+import MainFooter from "@/components/MainFooter";
+import MainHeader from "@/components/MainHeader";
+import NavBar from "@/components/NavBar";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+const NotFound: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>404 Not Found - MyAnimeList.net</title>
+        <meta name="keywords" content="anime, myanimelist, anime news, manga" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="keywords" content="anime, myanimelist, anime news, manga" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <MainHeader />
+      <NavBar />
+      <ContentTitleBar>404 Not Found</ContentTitleBar>
+      <MainContainer>
+        <div className="py-14 text-center">
+          <div className="flex justify-center">
+            <Link href="/">
+              <Image
+                src="https://cdn.myanimelist.net/images/error/404_image.png?v=170126"
+                alt="404 Not Found"
+                width={337}
+                height={191}
+                priority={true}
+                className="object-cover"
+              ></Image>
+            </Link>
+          </div>
+
+          <div className="font-[Helvetica] font-bold text-[#2e51a2] text-[2.4rem]">
+            This page doesn&apos;t exist.
+          </div>
+        </div>
+      </MainContainer>
+      <div className="h-36" />
+      <FooterChartPlaceholder />
+      <MainFooter />
+    </>
+  );
+};
+
+export default NotFound;
