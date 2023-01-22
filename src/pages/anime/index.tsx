5211177413ff33e54bb68c5f845dd59e7b750fa8
buzzind99 <buzzind99@gmail.com>
2023-01-22 21:22:07 +0700

    added anime page

---
+++ src/pages/anime/index.tsx
@@ -0,0 +1,30 @@
+import ContentTitleBar from "@/components/ContentTitleBar";
+import FooterChartPlaceholder from "@/components/FooterChartPlaceholder";
+import MainContainer from "@/components/MainContainer";
+import MainFooter from "@/components/MainFooter";
+import MainHeader from "@/components/MainHeader";
+import NavBar from "@/components/NavBar";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>MyAnimeList.net - Anime and Manga Database and Community</title>
+        <meta
+          name="description"
+          content="Welcome to MyAnimeList, the world&#039;s most active online anime and manga community and database. Join the online community, create your anime and manga list, read reviews, explore the forums, follow news, and so much more!"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="keywords" content="anime, myanimelist, anime news, manga" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <MainHeader />
+      <NavBar />
+      <ContentTitleBar>Anime</ContentTitleBar>
+      <MainContainer />
+      <FooterChartPlaceholder />
+      <MainFooter />
+    </>
+  );
+}
