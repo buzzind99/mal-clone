8032fe9fe720f2fcccfd3a8455d0bbcdcf89a0c3
buzzind99 <buzzind99@gmail.com>
2023-01-22 12:13:57 +0700

    added FooterChartPlaceholder component

---
+++ src/components/FooterChartPlaceholder.tsx
@@ -0,0 +1,133 @@
+const FooterChartPlaceholder: React.FC = () => {
+  return (
+    <div
+      id="footer-chart"
+      className="bg-[#f8f8f8] w-screen flex justify-center py-[1.25rem] font-[Verdana] tracking-tighter"
+    >
+      <div className="w-[12.5rem] mx-4 overflow-hidden">
+        <div className="border-[#dedede] border-b-[1px] pb-2 flex justify-between items-center">
+          <h3 className="text-[0.875rem] inline-block">Top Anime</h3>
+          <a className="text-[0.75rem] cursor-pointer text-[#1c439b] hover:underline">
+            More
+          </a>
+        </div>
+        <div className="tracking-tight">
+          <div className="mt-2 flex">
+            <span className=" text-[#808080] text-[0.8125rem]">1</span>
+            <a className="inline-block text-[0.625rem] ml-2 text-[#1c439b] hover:underline cursor-pointer mt-[0.1875rem]">
+              Bleach: Sennen Kessen-hen
+            </a>
+          </div>
+          <div className="flex mt-[0.1rem]">
+            <span className=" text-[#808080] text-[0.8125rem]">2</span>
+            <a className="inline-block text-[0.625rem] ml-2 text-[#1c439b] hover:underline cursor-pointer mt-[0.1875rem]">
+              Fullmetal Alchemist: Brotherhood
+            </a>
+          </div>
+          <div className="flex mt-[0.1rem]">
+            <span className=" text-[#808080] text-[0.8125rem]">3</span>
+            <a className="inline-block text-[0.625rem] ml-2 text-[#1c439b] hover:underline cursor-pointer mt-[0.1875rem]">
+              Steins;Gate
+            </a>
+          </div>
+          <div className="flex mt-[0.1rem]">
+            <span className=" text-[#808080] text-[0.8125rem]">4</span>
+            <a className="inline-block text-[0.625rem] ml-2 text-[#1c439b] hover:underline cursor-pointer mt-[0.1875rem]">
+              Gintama°
+            </a>
+          </div>
+          <div className="flex mt-[0.1rem]">
+            <span className=" text-[#808080] text-[0.8125rem]">5</span>
+            <a className="inline-block text-[0.625rem] ml-2 text-[#1c439b] hover:underline cursor-pointer mt-[0.1875rem]">
+              Kaguya-sama wa Kokurasetai: Ultra Romantic
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="w-[12.5rem] mx-4 overflow-hidden">
+        <div className="border-[#dedede] border-b-[1px] pb-2 flex justify-between items-center">
+          <h3 className="text-[0.875rem] inline-block">Top Airing Anime</h3>
+          <a className="text-[0.75rem] cursor-pointer text-[#1c439b] hover:underline">
+            More
+          </a>
+        </div>
+        <div className="tracking-tight">
+          <div className="mt-2 flex">
+            <span className=" text-[#808080] text-[0.8125rem]">1</span>
+            <a className="inline-block text-[0.625rem] ml-2 text-[#1c439b] hover:underline cursor-pointer mt-[0.1875rem]">
+              Vinland Saga Season 2
+            </a>
+          </div>
+          <div className="flex mt-[0.1rem]">
+            <span className=" text-[#808080] text-[0.8125rem]">2</span>
+            <a className="inline-block text-[0.625rem] ml-2 text-[#1c439b] hover:underline cursor-pointer mt-[0.1875rem]">
+              One Piece
+            </a>
+          </div>
+          <div className="flex mt-[0.1rem]">
+            <span className=" text-[#808080] text-[0.8125rem]">3</span>
+            <a className="inline-block text-[0.625rem] ml-2 text-[#1c439b] hover:underline cursor-pointer mt-[0.1875rem]">
+              Bungou Stray Dogs 4th Season
+            </a>
+          </div>
+          <div className="flex mt-[0.1rem]">
+            <span className=" text-[#808080] text-[0.8125rem]">4</span>
+            <a className="inline-block text-[0.625rem] ml-2 text-[#1c439b] hover:underline cursor-pointer mt-[0.1875rem]">
+              Golden Kamuy 4th Season
+            </a>
+          </div>
+          <div className="flex mt-[0.1rem]">
+            <span className=" text-[#808080] text-[0.8125rem]">5</span>
+            <a className="inline-block text-[0.625rem] ml-2 text-[#1c439b] hover:underline cursor-pointer mt-[0.1875rem]">
+              Holo no Graffiti
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="w-[12.5rem] mx-4 overflow-hidden">
+        <div className="border-[#dedede] border-b-[1px] pb-2 flex justify-between items-center">
+          <h3 className="text-[0.875rem] inline-block">
+            Most Popular Characters
+          </h3>
+          <a className="text-[0.75rem] cursor-pointer text-[#1c439b] hover:underline">
+            More
+          </a>
+        </div>
+        <div className="tracking-tight">
+          <div className="mt-2 flex">
+            <span className=" text-[#808080] text-[0.8125rem]">1</span>
+            <a className="inline-block text-[0.625rem] ml-2 text-[#1c439b] hover:underline cursor-pointer mt-[0.1875rem]">
+              Lamperouge, Lelouch
+            </a>
+          </div>
+          <div className="flex mt-[0.1rem]">
+            <span className=" text-[#808080] text-[0.8125rem]">2</span>
+            <a className="inline-block text-[0.625rem] ml-2 text-[#1c439b] hover:underline cursor-pointer mt-[0.1875rem]">
+              Levi
+            </a>
+          </div>
+          <div className="flex mt-[0.1rem]">
+            <span className=" text-[#808080] text-[0.8125rem]">3</span>
+            <a className="inline-block text-[0.625rem] ml-2 text-[#1c439b] hover:underline cursor-pointer mt-[0.1875rem]">
+              Monkey D., Luffy
+            </a>
+          </div>
+          <div className="flex mt-[0.1rem]">
+            <span className=" text-[#808080] text-[0.8125rem]">4</span>
+            <a className="inline-block text-[0.625rem] ml-2 text-[#1c439b] hover:underline cursor-pointer mt-[0.1875rem]">
+              Lawliet, L
+            </a>
+          </div>
+          <div className="flex mt-[0.1rem]">
+            <span className=" text-[#808080] text-[0.8125rem]">5</span>
+            <a className="inline-block text-[0.625rem] ml-2 text-[#1c439b] hover:underline cursor-pointer mt-[0.1875rem]">
+              Roronoa, Zoro
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FooterChartPlaceholder;
