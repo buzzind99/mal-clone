b32bcb011d5b4b4edff8209293c29cc79432dfb1
buzzind99 <buzzind99@gmail.com>
2023-01-22 12:16:29 +0700

    added TopUpcomingAnimePlaceholder component

---
+++ src/pages/components/TopUpcomingAnimePlaceholder copy.tsx
@@ -0,0 +1,189 @@
+import Image from "next/image";
+
+const TopUpcomingAnimePlaceholder: React.FC = () => {
+  return (
+    <article
+      id="top-upcoming"
+      className="w-[18.875rem] bg-[#f8f8f8] justify-between overflow-hidden"
+    >
+      <div
+        id="top-upcoming_header"
+        className="mb-[0.3125rem] h-[1.75rem] flex items-center justify-between bg-[#dde1ec] px-[0.5rem] w-full font-bold font-[Verdana]"
+      >
+        <h2 className="text-[0.875rem] tracking-tighter">
+          Top Upcoming Anime {"{dummy}"}
+        </h2>
+        <a className="cursor-pointer text-[0.75rem] text-[#1c439b] hover:underline leading-tight tracking-tight">
+          More
+        </a>
+      </div>
+      <div className="pt-4 pb-[1.25rem] flex px-2">
+        <span className="text-[#808080] font-bold font-[Verdana] h-[4.375rem] w-[1.5rem] align-top leading-none">
+          1
+        </span>
+        <a className="h-[4.5rem] w-[3.425rem] inline-block border-[1px] border-[#bebebe] cursor-pointer">
+          <Image
+            src="https://cdn.myanimelist.net/images/anime/1804/126632.jpg"
+            alt="placeholder"
+            width={225}
+            height={319}
+            priority={true}
+            className="h-[4.375rem]"
+          />
+        </a>
+        <div className="ml-2 w-[13.25rem]">
+          <div className="flex justify-between font-[Verdana] leading-tight tracking-tight text-[#1c439b] align-top">
+            <h3 className="inline-block cursor-pointer hover:underline text-[0.75rem] font-bold max-w-[11.0625rem]">
+              Tate no Yuusha no Nariagari Season 3
+            </h3>
+            <span className="text-[0.625rem] bg-[#f0f0f0] w-[1.5rem] h-[0.875rem] pl-[0.225rem] pt-[0.05rem] hover:text-white cursor-pointer transition-all duration-300 border-b-[1px] border-[#e5e5e5]">
+              add
+            </span>
+          </div>
+          <div className="flex-col mt-[0.4rem]">
+            <div className="text-[#808080] text-[0.75rem]">
+              TV, 0 eps, scored N/A
+            </div>
+            <div className="text-[#808080] text-[0.75rem] leading-none">
+              268,269 members
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="pb-[1.25rem] flex px-2">
+        <span className="text-[#808080] font-bold font-[Verdana] h-[4.375rem] w-[1.5rem] align-top leading-none">
+          2
+        </span>
+        <a className="h-[4.5rem] w-[3.425rem] inline-block border-[1px] border-[#bebebe] cursor-pointer">
+          <Image
+            src="https://cdn.myanimelist.net/images/anime/1361/130998.jpg"
+            alt="placeholder"
+            width={225}
+            height={318}
+            priority={true}
+            className="h-[4.375rem]"
+          />
+        </a>
+        <div className="ml-2 w-[13.25rem]">
+          <div className="flex justify-between font-[Verdana] leading-tight tracking-tight text-[#1c439b] align-top">
+            <h3 className="inline-block cursor-pointer hover:underline text-[0.75rem] font-bold max-w-[11.0625rem]">
+              Shingeki no Kyojin: The Final Season - Kanketsu-hen
+            </h3>
+            <span className="text-[0.625rem] bg-[#f0f0f0] w-[1.5rem] h-[0.875rem] pl-[0.225rem] pt-[0.05rem] hover:text-white cursor-pointer transition-all duration-300 border-b-[1px] border-[#e5e5e5]">
+              add
+            </span>
+          </div>
+          <div className="flex-col mt-[0.4rem]">
+            <div className="text-[#808080] text-[0.75rem]">
+              TV, 0 eps, scored N/A
+            </div>
+            <div className="text-[#808080] text-[0.75rem] leading-none">
+              234,644 members
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="pb-[1.25rem] flex px-2">
+        <span className="text-[#808080] font-bold font-[Verdana] h-[4.375rem] w-[1.5rem] align-top leading-none">
+          3
+        </span>
+        <a className="h-[4.5rem] w-[3.425rem] inline-block border-[1px] border-[#bebebe] cursor-pointer">
+          <Image
+            src="https://cdn.myanimelist.net/images/anime/1499/121027.jpg"
+            alt="placeholder"
+            width={225}
+            height={314}
+            priority={true}
+            className="h-[4.375rem]"
+          />
+        </a>
+        <div className="ml-2 w-[13.25rem]">
+          <div className="flex justify-between font-[Verdana] leading-tight tracking-tight text-[#1c439b] align-top">
+            <h3 className="inline-block cursor-pointer hover:underline text-[0.75rem] font-bold max-w-[11.0625rem]">
+              Kimetsu no Yaiba: Katanakaji no Sato-hen
+            </h3>
+            <span className="text-[0.625rem] bg-[#f0f0f0] w-[1.5rem] h-[0.875rem] pl-[0.225rem] pt-[0.05rem] hover:text-white cursor-pointer transition-all duration-300 border-b-[1px] border-[#e5e5e5]">
+              add
+            </span>
+          </div>
+          <div className="flex-col mt-[0.4rem]">
+            <div className="text-[#808080] text-[0.75rem]">
+              TV, 0 eps, scored N/A
+            </div>
+            <div className="text-[#808080] text-[0.75rem] leading-none">
+              191,012 members
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="pb-[1.25rem] flex px-2">
+        <span className="text-[#808080] font-bold font-[Verdana] h-[4.375rem] w-[1.5rem] align-top leading-none">
+          4
+        </span>
+        <a className="h-[4.5rem] w-[3.425rem] inline-block border-[1px] border-[#bebebe] cursor-pointer">
+          <Image
+            src="https://cdn.myanimelist.net/images/anime/1676/128448.jpg"
+            alt="placeholder"
+            width={225}
+            height={318}
+            priority={true}
+            className="h-[4.375rem]"
+          />
+        </a>
+        <div className="ml-2 w-[13.25rem]">
+          <div className="flex justify-between font-[Verdana] leading-tight tracking-tight text-[#1c439b] align-top">
+            <h3 className="inline-block cursor-pointer hover:underline text-[0.75rem] font-bold max-w-[11.0625rem]">
+              Jujutsu Kaisen 2nd Season
+            </h3>
+            <span className="text-[0.625rem] bg-[#f0f0f0] w-[1.5rem] h-[0.875rem] pl-[0.225rem] pt-[0.05rem] hover:text-white cursor-pointer transition-all duration-300 border-b-[1px] border-[#e5e5e5]">
+              add
+            </span>
+          </div>
+          <div className="flex-col mt-[0.4rem]">
+            <div className="text-[#808080] text-[0.75rem]">
+              TV, 0 eps, scored N/A
+            </div>
+            <div className="text-[#808080] text-[0.75rem] leading-none">
+              183,677 members
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="pb-[1.25rem] flex px-2">
+        <span className="text-[#808080] font-bold font-[Verdana] h-[4.375rem] w-[1.5rem] align-top leading-none">
+          5
+        </span>
+        <a className="h-[4.5rem] w-[3.425rem] inline-block border-[1px] border-[#bebebe] cursor-pointer">
+          <Image
+            src="https://cdn.myanimelist.net/images/anime/1785/131775.jpg"
+            alt="placeholder"
+            width={225}
+            height={318}
+            priority={true}
+            className="h-[4.375rem]"
+          />
+        </a>
+        <div className="ml-2 w-[13.25rem]">
+          <div className="flex justify-between font-[Verdana] leading-tight tracking-tight text-[#1c439b] align-top">
+            <h3 className="inline-block cursor-pointer hover:underline text-[0.75rem] font-bold max-w-[11.0625rem]">
+              Dr. Stone: New World
+            </h3>
+            <span className="text-[0.625rem] bg-[#f0f0f0] w-[1.5rem] h-[0.875rem] pl-[0.225rem] pt-[0.05rem] hover:text-white cursor-pointer transition-all duration-300 border-b-[1px] border-[#e5e5e5]">
+              add
+            </span>
+          </div>
+          <div className="flex-col mt-[0.4rem]">
+            <div className="text-[#808080] text-[0.75rem]">
+              TV, 0 eps, scored N/A
+            </div>
+            <div className="text-[#808080] text-[0.75rem] leading-none">
+              169,917 members
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default TopUpcomingAnimePlaceholder;
