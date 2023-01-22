d7e8d6f58dcafb52aae9abcafcaf9dd6c8af2eb3
buzzind99 <buzzind99@gmail.com>
2023-01-22 12:16:39 +0700

    added TopAiringAnimePlaceholder component

---
+++ src/pages/components/TopAiringAnimePlaceholder.tsx
@@ -0,0 +1,189 @@
+import Image from "next/image";
+
+const TopAiringAnimePlaceholder: React.FC = () => {
+  return (
+    <article
+      id="top-airing"
+      className="w-[18.875rem] bg-[#f8f8f8] justify-between overflow-hidden"
+    >
+      <div
+        id="top-airing_header"
+        className="mb-[0.3125rem] h-[1.75rem] flex items-center justify-between bg-[#dde1ec] px-[0.5rem] w-full font-bold font-[Verdana]"
+      >
+        <h2 className="text-[0.875rem] tracking-tighter">
+          Top Airing Anime {"{dummy}"}
+        </h2>
+        <a className="cursor-pointer text-[0.75rem] text-[#1c439b] hover:underline leading-tight tracking-tight">
+          More
+        </a>
+      </div>
+      <div className="pt-4 pb-[1.25rem] flex px-2">
+        <span className="text-[#808080] font-bold font-[Verdana] h-[4.375rem] w-[1.5rem] align-top leading-none">
+          1
+        </span>
+        <a className="h-[4.5rem] w-[3.425rem] inline-block border-[1px] border-[#bebebe] cursor-pointer">
+          <Image
+            src="https://cdn.myanimelist.net/images/anime/1170/124312.jpg"
+            alt="placeholder"
+            width={225}
+            height={318}
+            priority={true}
+            className="h-[4.375rem]"
+          />
+        </a>
+        <div className="ml-2 w-[13.25rem]">
+          <div className="flex justify-between font-[Verdana] leading-tight tracking-tight text-[#1c439b] align-top">
+            <h3 className="inline-block cursor-pointer hover:underline text-[0.75rem] font-bold max-w-[11.0625rem]">
+              Vinland Saga Season 2
+            </h3>
+            <span className="text-[0.625rem] bg-[#f0f0f0] w-[1.5rem] h-[0.875rem] pl-[0.225rem] pt-[0.05rem] hover:text-white cursor-pointer transition-all duration-300 border-b-[1px] border-[#e5e5e5]">
+              add
+            </span>
+          </div>
+          <div className="flex-col mt-[0.4rem]">
+            <div className="text-[#808080] text-[0.75rem]">
+              TV, 24 eps, scored 8.83
+            </div>
+            <div className="text-[#808080] text-[0.75rem] leading-none">
+              275,563 members
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="pb-[1.25rem] flex px-2">
+        <span className="text-[#808080] font-bold font-[Verdana] h-[4.375rem] w-[1.5rem] align-top leading-none">
+          2
+        </span>
+        <a className="h-[4.5rem] w-[3.425rem] inline-block border-[1px] border-[#bebebe] cursor-pointer">
+          <Image
+            src="https://cdn.myanimelist.net/images/anime/6/73245.jpg"
+            alt="placeholder"
+            width={225}
+            height={350}
+            priority={true}
+            className="h-[4.375rem]"
+          />
+        </a>
+        <div className="ml-2 w-[13.25rem]">
+          <div className="flex justify-between font-[Verdana] leading-tight tracking-tight text-[#1c439b] align-top">
+            <h3 className="inline-block cursor-pointer hover:underline text-[0.75rem] font-bold max-w-[11.0625rem]">
+              One Piece
+            </h3>
+            <span className="text-[0.625rem] bg-[#f0f0f0] w-[1.5rem] h-[0.875rem] pl-[0.225rem] pt-[0.05rem] hover:text-white cursor-pointer transition-all duration-300 border-b-[1px] border-[#e5e5e5]">
+              add
+            </span>
+          </div>
+          <div className="flex-col mt-[0.4rem]">
+            <div className="text-[#808080] text-[0.75rem]">
+              TV, 0 eps, scored 8.68
+            </div>
+            <div className="text-[#808080] text-[0.75rem] leading-none">
+              2,075,454 members
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="pb-[1.25rem] flex px-2">
+        <span className="text-[#808080] font-bold font-[Verdana] h-[4.375rem] w-[1.5rem] align-top leading-none">
+          3
+        </span>
+        <a className="h-[4.5rem] w-[3.425rem] inline-block border-[1px] border-[#bebebe] cursor-pointer">
+          <Image
+            src="https://cdn.myanimelist.net/images/anime/1720/126608.jpg"
+            alt="placeholder"
+            width={225}
+            height={318}
+            priority={true}
+            className="h-[4.375rem]"
+          />
+        </a>
+        <div className="ml-2 w-[13.25rem]">
+          <div className="flex justify-between font-[Verdana] leading-tight tracking-tight text-[#1c439b] align-top">
+            <h3 className="inline-block cursor-pointer hover:underline text-[0.75rem] font-bold max-w-[11.0625rem]">
+              Bungou Stray Dogs 4th Season
+            </h3>
+            <span className="text-[0.625rem] bg-[#f0f0f0] w-[1.5rem] h-[0.875rem] pl-[0.225rem] pt-[0.05rem] hover:text-white cursor-pointer transition-all duration-300 border-b-[1px] border-[#e5e5e5]">
+              add
+            </span>
+          </div>
+          <div className="flex-col mt-[0.4rem]">
+            <div className="text-[#808080] text-[0.75rem]">
+              TV, 13 eps, scored 8.58
+            </div>
+            <div className="text-[#808080] text-[0.75rem] leading-none">
+              160,740 members
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="pb-[1.25rem] flex px-2">
+        <span className="text-[#808080] font-bold font-[Verdana] h-[4.375rem] w-[1.5rem] align-top leading-none">
+          4
+        </span>
+        <a className="h-[4.5rem] w-[3.425rem] inline-block border-[1px] border-[#bebebe] cursor-pointer">
+          <Image
+            src="https://cdn.myanimelist.net/images/anime/1855/128059.jpg"
+            alt="placeholder"
+            width={225}
+            height={318}
+            priority={true}
+            className="h-[4.375rem]"
+          />
+        </a>
+        <div className="ml-2 w-[13.25rem]">
+          <div className="flex justify-between font-[Verdana] leading-tight tracking-tight text-[#1c439b] align-top">
+            <h3 className="inline-block cursor-pointer hover:underline text-[0.75rem] font-bold max-w-[11.0625rem]">
+              Golden Kamuy 4th Season
+            </h3>
+            <span className="text-[0.625rem] bg-[#f0f0f0] w-[1.5rem] h-[0.875rem] pl-[0.225rem] pt-[0.05rem] hover:text-white cursor-pointer transition-all duration-300 border-b-[1px] border-[#e5e5e5]">
+              add
+            </span>
+          </div>
+          <div className="flex-col mt-[0.4rem]">
+            <div className="text-[#808080] text-[0.75rem]">
+              TV, 13 eps, scored 8.28
+            </div>
+            <div className="text-[#808080] text-[0.75rem] leading-none">
+              67,167 members
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="pb-[1.25rem] flex px-2">
+        <span className="text-[#808080] font-bold font-[Verdana] h-[4.375rem] w-[1.5rem] align-top leading-none">
+          5
+        </span>
+        <a className="h-[4.5rem] w-[3.425rem] inline-block border-[1px] border-[#bebebe] cursor-pointer">
+          <Image
+            src="https://cdn.myanimelist.net/images/anime/1259/110227.jpg"
+            alt="placeholder"
+            width={225}
+            height={312}
+            priority={true}
+            className="h-[4.375rem]"
+          />
+        </a>
+        <div className="ml-2 w-[13.25rem]">
+          <div className="flex justify-between font-[Verdana] leading-tight tracking-tight text-[#1c439b] align-top">
+            <h3 className="inline-block cursor-pointer hover:underline text-[0.75rem] font-bold max-w-[11.0625rem]">
+              Holo no Graffiti
+            </h3>
+            <span className="text-[0.625rem] bg-[#f0f0f0] w-[1.5rem] h-[0.875rem] pl-[0.225rem] pt-[0.05rem] hover:text-white cursor-pointer transition-all duration-300 border-b-[1px] border-[#e5e5e5]">
+              add
+            </span>
+          </div>
+          <div className="flex-col mt-[0.4rem]">
+            <div className="text-[#808080] text-[0.75rem]">
+              ONA, 0 eps, scored 8.28
+            </div>
+            <div className="text-[#808080] text-[0.75rem] leading-none">
+              50,270 members
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default TopAiringAnimePlaceholder;
