3f2d77448ec84553ce5aec5666627ba45e418c17
buzzind99 <buzzind99@gmail.com>
2023-01-22 12:17:57 +0700

    changed styling

--- src/pages/components/MALxJPN.tsx
+++ src/pages/components/MALxJPN.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 
-const MALxJPN = () => {
+const MALxJPN: React.FC= () => {
   return (
     <article
       id="mal-x-jpn"
-      className="mx-[0.625rem] mb-[1.25rem] font-[Verdana] font-bold text-[0.75rem]"
+      className="font-[Verdana] font-bold text-[0.75rem]"
     >
       <div
         id="mal-x-jpn_header"
         className="h-[1.375rem] py-[0.1875rem] border-b-[1px] border-[#bebebe] flex justify-between items-center"
       >
-        <span className="float-left">MALxJapan -More than just anime-</span>
+        <span className="float-left">MALxJapan -More than just anime- {"{this is a dummy}"}</span>
         <a
           href="https://mxj.myanimelist.net/"
           target="https://mxj.myanimelist.net/"
@@ -19,7 +19,7 @@ const MALxJPN = () => {
           Visit MALxJapan
         </a>
       </div>
-      <div id="mal-x-japan_content" className="flex justify-between mt-1">
+      <div id="mal-x-japan_content" className="flex mt-1">
         <a
           id="mal-x-japan_content_1"
           title="Learn how to draw anime &amp; manga from Japanese pros🎨"
@@ -44,7 +44,7 @@ const MALxJPN = () => {
           title="How much of an otaku are you? Take the quiz!"
           href="https://mxj.myanimelist.net/otakujudge/takequiz/?utm_source=MAL&utm_medium=top_mxj_otakujudge-app"
           target="https://mxj.myanimelist.net/otakujudge/takequiz/?utm_source=MAL&utm_medium=top_mxj_otakujudge-app"
-          className="w-[14.6875rem] cursor-pointer text-[0.6875rem] leading-tight group"
+          className="w-[14.6875rem] mx-[0.4375rem] cursor-pointer text-[0.6875rem] leading-tight group"
         >
           <Image
             src="/mal-x-jpn_card2.webp"
