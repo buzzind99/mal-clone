7604f837d7c9dfcc8a48708cba9f17461e332ff0
buzzind99 <buzzind99@gmail.com>
2023-01-23 19:32:00 +0700

    changed props

--- src/components/ContentTitleBar.tsx
+++ src/components/ContentTitleBar.tsx
@@ -1,14 +1,15 @@
-type Props = {
+interface Props {
   children?: React.ReactNode;
+  title: string;
 };
 
-const ContentTitleBar: React.FC<Props> = ({ children }) => {
+const ContentTitleBar: React.FC<Props> = ({ title }) => {
   return (
     <h1
       id="content-title"
       className="mx-auto flex w-[66.25rem] items-center border-b-[1px] border-[#2e51a2] bg-[#e1e7f5] px-[0.625rem] pt-[0.25rem] pb-[0.125rem] font-[Verdana] font-bold"
     >
-      {children}
+      {title}
     </h1>
   );
 };
