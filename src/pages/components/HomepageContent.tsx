254c670acea197afda431e5f7633916942f49311
buzzind99 <buzzind99@gmail.com>
2023-01-23 13:16:30 +0700

    added AnimeMangaNews to homepage

--- src/pages/components/HomepageContent.tsx
+++ src/pages/components/HomepageContent.tsx
@@ -1,3 +1,4 @@
+import AnimeMangaNews from "./AnimeMangaNews";
 import MALxJPN from "./MALxJPN";
 import MostPopularAnimePlaceholder from "./MostPopularAnimePlaceholder";
 import TopAiringAnimePlaceholder from "./TopAiringAnimePlaceholder";
@@ -5,13 +6,16 @@ import TopUpcomingAnimePlaceholder from "./TopUpcomingAnimePlaceholder";
 
 const HomepageContent: React.FC = () => {
   return (
-    <div className="bg-white relative text-left flex py-[0.625rem]">
+    <div className="relative flex bg-white py-[0.625rem] text-left">
       <div
         id="content-left"
         className="inline-block w-[46.5395rem] border-r-[1px] border-[#e5e5e5]"
       >
         <div className="mx-[0.625rem] mb-[1.25rem]">
           <MALxJPN />
+        </div>
+        <div className="mx-[0.625rem] mb-[1.25rem]">
+          <AnimeMangaNews />
         </div>
       </div>
       <div id="content-right" className="inline-block w-[20rem]">
