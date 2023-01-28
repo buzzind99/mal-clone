7e4da278ab212b657d9bfdf0e6212ebe72adeda8
buzzind99 <buzzind99@gmail.com>
2023-01-28 18:44:49 +0700

    fixed width

--- src/components/ContentTitleBar.tsx
+++ src/components/ContentTitleBar.tsx
@@ -7,7 +7,7 @@ const ContentTitleBar: React.FC<Props> = ({ title }) => {
   return (
     <h1
       id="content-title"
-      className="mx-auto flex w-[66.25rem] items-center border-b-[1px] border-[#2e51a2] bg-[#e1e7f5] px-[0.625rem] pt-[0.25rem] pb-[0.125rem] font-[Verdana] font-bold"
+      className="mx-auto flex w-[66.375rem] items-center border-b-[1px] border-[#2e51a2] bg-[#e1e7f5] px-[0.625rem] pt-[0.25rem] pb-[0.125rem] font-[Verdana] font-bold"
     >
       {title}
     </h1>
