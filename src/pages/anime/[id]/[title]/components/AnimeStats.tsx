c501a8bab5620a3845c6af77b32252c98e45901c
buzzind99 <buzzind99@gmail.com>
2023-02-05 20:39:35 +0700

    added initial data

--- src/pages/anime/[id]/[title]/components/AnimeStats.tsx
+++ src/pages/anime/[id]/[title]/components/AnimeStats.tsx
@@ -1,3 +1,4 @@
+import { initialAnimeData } from "@/types/initializer";
 import { IAnimeData } from "@/types/interfaces";
 
 interface Props {
@@ -5,27 +6,29 @@ interface Props {
 }
 
 const AnimeStats: React.FC<Props> = ({ animeData }) => {
+  const data = animeData || initialAnimeData;
+
   return (
     <div id="statistics" className="mb-5">
       <h2
         id="statistics_header"
-        className="mb-[0.45rem] border-b-[1px] border-[#bebebe] py-[0.15rem] text-[0.75rem] font-bold "
+        className="mb-[0.45rem] border-b-[1px] border-[#bebebe] py-[0.15rem] text-[0.75rem] font-bold"
       >
         Statistics
       </h2>
       <div id="statistics_score" className="my-[0.375rem] text-[0.6875rem]">
         <span className="font-bold text-[#444]">{"Score: "}</span>
-        {animeData.score || "N/A"}
+        {data.score || "N/A"}
         <sup className="text-[0.5625rem] text-black">1</sup>
         {` (scored by ${
-          animeData.scored_by
-            ? new Intl.NumberFormat("en").format(+animeData.scored_by)
+          data.scored_by
+            ? new Intl.NumberFormat("en").format(+data.scored_by)
             : "-"
         } users)`}
       </div>
       <div id="statistics_ranked" className="my-[0.375rem] text-[0.6875rem]">
         <span className="font-bold text-[#444]">{"Ranked: "}</span>
-        {`#${animeData.rank || "N/A"}`}
+        {`#${data.rank || "N/A"}`}
         <sup className="text-[0.5625rem] text-black">2</sup>
       </div>
       <div
@@ -33,18 +36,18 @@ const AnimeStats: React.FC<Props> = ({ animeData }) => {
         className="my-[0.375rem] text-[0.6875rem]"
       >
         <span className="font-bold text-[#444]">{"Popularity: "}</span>
-        {`#${animeData.popularity || "N/A"}`}
+        {`#${data.popularity || "N/A"}`}
       </div>
       <div id="statistics_members" className="my-[0.375rem] text-[0.6875rem]">
         <span className="font-bold text-[#444]">{"Members: "}</span>
-        {animeData.members
-          ? new Intl.NumberFormat("en").format(+animeData.members)
+        {data.members
+          ? new Intl.NumberFormat("en").format(+data.members)
           : "N/A"}
       </div>
       <div id="statistics_favorites" className="my-[0.375rem] text-[0.6875rem]">
         <span className="font-bold text-[#444]">{"Members: "}</span>
-        {animeData.favorites
-          ? new Intl.NumberFormat("en").format(+animeData.favorites)
+        {data.favorites
+          ? new Intl.NumberFormat("en").format(+data.favorites)
           : "N/A"}
       </div>
     </div>
