dff2e5619b988d359d3b542c8d8697a7a8e0df3b
buzzind99 <buzzind99@gmail.com>
2023-02-05 16:58:28 +0700

    added option for sub title

--- src/components/ContentTitleBar.tsx
+++ src/components/ContentTitleBar.tsx
@@ -1,15 +1,18 @@
 interface Props {
-  children?: React.ReactNode;
   title: string;
+  title_english?: string;
 }
 
-const ContentTitleBar: React.FC<Props> = ({ title }) => {
+const ContentTitleBar: React.FC<Props> = ({ title, title_english }) => {
   return (
     <h1
       id="content-title"
-      className="mx-auto flex w-[66.375rem] items-center border-b-[1px] border-[#2e51a2] bg-[#e1e7f5] px-[0.625rem] pt-[0.25rem] pb-[0.125rem] font-[Verdana] font-bold"
+      className="mx-auto block w-[66.375rem] items-center border-b-[1px] border-[#2e51a2] bg-[#e1e7f5] px-[0.625rem] pt-[0.25rem] pb-[0.125rem] font-[Verdana] font-bold"
     >
       {title}
+      {title_english && <p className="mb-1 block text-[0.875rem] leading-none text-[grey]">
+        {title_english}
+      </p>}
     </h1>
   );
 };
