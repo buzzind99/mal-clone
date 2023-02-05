b46ffae7a11362bebc231aa1f2b5b8c531b86c1c
buzzind99 <buzzind99@gmail.com>
2023-02-05 22:16:47 +0700

    added initial data

--- src/pages/anime/[id]/[title]/components/AnimeRelations.tsx
+++ src/pages/anime/[id]/[title]/components/AnimeRelations.tsx
@@ -12,7 +12,13 @@ interface Props {
   }[];
 }
 
+const initialData = [
+  { relation: "", entry: [{ mal_id: 0, type: "", name: "", url: "" }] },
+];
+
 const AnimeRelations: React.FC<Props> = ({ relations }) => {
+  const data = relations || initialData;
+
   return (
     <div id="relations" className="mb-5">
       <h2
@@ -28,8 +34,8 @@ const AnimeRelations: React.FC<Props> = ({ relations }) => {
         id="relations_content"
         className="table whitespace-pre-line text-[0.6875rem]"
       >
-        {relations
-          ? relations.map((relation) => (
+        {data
+          ? data.map((relation) => (
               <tr key={relation.relation} className="table-row">
                 <td className="whitespace-nowrap border-b-[1px] border-[#e5e5e5] p-[0.1875rem] text-right">{`${relation.relation}:`}</td>
                 <td className="w-full border-b-[1px] border-[#e5e5e5] pl-[0.1875rem]">
