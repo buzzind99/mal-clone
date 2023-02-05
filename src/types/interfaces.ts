fbd0e02848c87d47865eb50fa2ab3ddd672d67ca
buzzind99 <buzzind99@gmail.com>
2023-02-05 16:59:17 +0700

    added interface

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -86,3 +86,138 @@ export interface IPopularTrailersData {
   video_url: string;
   type: string;
 }
+
+export interface IAnimeData {
+  mal_id: number;
+  url: string;
+  images: {
+    jpg: {
+      image_url: string | null;
+      small_image_url: string | null;
+      large_image_url: string | null;
+    };
+    webp: {
+      image_url: string | null;
+      small_image_url: string | null;
+      large_image_url: string | null;
+    };
+  };
+  trailer: {
+    youtube_id: string | null;
+    url: string | null;
+    embed_url: string | null;
+  };
+  approved: boolean;
+  titles: {
+    type: string;
+    title: string;
+  }[];
+  title: string;
+  title_english: string | null;
+  title_japanese: string | null;
+  title_synonyms: string[] | null;
+  type: string | null;
+  source: string | null;
+  episodes: number | null;
+  status: string | null;
+  airing: boolean;
+  aired: {
+    from: string | null;
+    to: string | null;
+    prop: {
+      from: {
+        day: number | null;
+        month: number | null;
+        year: number | null;
+      };
+      to: {
+        day: number | null;
+        month: number | null;
+        year: number | null;
+      };
+      string: string | null;
+    };
+    string: string | null;
+  };
+  duration: string | null;
+  rating: string | null;
+  score: number | null;
+  scored_by: number | null;
+  rank: number | null;
+  popularity: number | null;
+  members: number | null;
+  favorites: number | null;
+  synopsis: string | null;
+  background: string | null;
+  season: string | null;
+  year: number | null;
+  broadcast: {
+    day: string | null;
+    time: string | null;
+    timezone: string | null;
+    string: string | null;
+  };
+  producers: {
+    mal_id: number;
+    type: string;
+    name: string;
+    url: string;
+  }[];
+  licensors: {
+    mal_id: number;
+    type: string;
+    name: string;
+    url: string;
+  }[];
+  studios: {
+    mal_id: number;
+    type: string;
+    name: string;
+    url: string;
+  }[];
+  genres: {
+    mal_id: number;
+    type: string;
+    name: string;
+    url: string;
+  }[];
+  explicit_genres: {
+    mal_id: number;
+    type: string;
+    name: string;
+    url: string;
+  }[];
+  themes: {
+    mal_id: number;
+    type: string;
+    name: string;
+    url: string;
+  }[];
+  demographics: {
+    mal_id: number;
+    type: string;
+    name: string;
+    url: string;
+  }[];
+  relations: {
+    relation: string;
+    entry: {
+      mal_id: number;
+      type: string;
+      name: string;
+      url: string;
+    }[];
+  }[];
+  theme: {
+    openings: string[];
+    endings: string[];
+  };
+  external: {
+    name: string;
+    url: string;
+  }[];
+  streaming: {
+    name: string;
+    url: string;
+  }[];
+}
