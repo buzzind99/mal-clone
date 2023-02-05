c01aa2c83e2af63e67d4155bd5a0ac2e4fbc1510
buzzind99 <buzzind99@gmail.com>
2023-02-05 20:38:56 +0700

    added AnimeBackground component

---
+++ src/pages/anime/[id]/[title]/components/AnimeBackground.tsx
@@ -0,0 +1,28 @@
+interface Props {
+  background: string | null;
+}
+
+const AnimeBackground: React.FC<Props> = ({ background }) => {
+  return (
+    <div id="background" className="mb-5">
+      <h2
+        id="background_header"
+        className="mb-[0.45rem] flex w-full justify-between border-b-[1px] border-[#bebebe] py-[0.15rem] text-[0.75rem] font-bold"
+      >
+        Background
+        <span className="cursor-pointer text-[0.6875rem] font-normal text-[#1c439b] hover:underline">
+          Edit
+        </span>
+      </h2>
+      <div
+        id="background_content"
+        className="whitespace-pre-line text-[0.6875rem]"
+      >
+        {background ||
+          "No background information has been added to this title."}
+      </div>
+    </div>
+  );
+};
+
+export default AnimeBackground;
