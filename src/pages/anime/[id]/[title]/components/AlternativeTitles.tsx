a156ba9dff527663aec17f0d3d103ce9bad16c13
buzzind99 <buzzind99@gmail.com>
2023-02-05 16:59:50 +0700

    added AlternativeTitles component

---
+++ src/pages/anime/[id]/[title]/components/AlternativeTitles.tsx
@@ -0,0 +1,33 @@
+interface Props {
+  titles: {
+    type: string;
+    title: string;
+  }[];
+}
+
+const AlternativeTitles: React.FC<Props> = ({ titles }) => {
+  return (
+    <div id="alternative-titles" className="mb-5">
+      <h2
+        id="alternative-titles_header"
+        className="mb-[0.45rem] border-b-[1px] border-[#bebebe] py-[0.15rem] text-[0.75rem] font-bold "
+      >
+        Alternative Titles
+      </h2>
+      {titles.slice(1).map((title) => (
+        <div
+          id={`titles_${title.type.toLowerCase()}`}
+          key={title.type}
+          className="my-[0.375rem] text-[0.6875rem]"
+        >
+          <span className="font-bold text-[#444]">{`${
+            title.type === "Synonym" ? "Synonyms" : title.type
+          }: `}</span>
+          {title.title}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AlternativeTitles;
