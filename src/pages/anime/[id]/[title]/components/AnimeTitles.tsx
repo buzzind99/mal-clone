48e8e9c88be0890d995a5d3ced320210cd79810b
buzzind99 <buzzind99@gmail.com>
2023-02-05 20:40:20 +0700

    added initial data

--- src/pages/anime/[id]/[title]/components/AnimeTitles.tsx
+++ src/pages/anime/[id]/[title]/components/AnimeTitles.tsx
@@ -5,16 +5,25 @@ interface Props {
   }[];
 }
 
+const initialData: Props = {
+  titles: [
+    { type: "", title: "" },
+    { type: "", title: "" },
+  ],
+};
+
 const AnimeTitles: React.FC<Props> = ({ titles }) => {
+  const data = titles || initialData.titles;
+
   return (
     <div id="alternative-titles" className="mb-5">
       <h2
         id="alternative-titles_header"
-        className="mb-[0.45rem] border-b-[1px] border-[#bebebe] py-[0.15rem] text-[0.75rem] font-bold "
+        className="mb-[0.45rem] border-b-[1px] border-[#bebebe] py-[0.15rem] text-[0.75rem] font-bold"
       >
         Alternative Titles
       </h2>
-      {titles.slice(1).map((title) => (
+      {data.slice(1).map((title) => (
         <div
           id={`titles_${title.type.toLowerCase()}`}
           key={title.type}
