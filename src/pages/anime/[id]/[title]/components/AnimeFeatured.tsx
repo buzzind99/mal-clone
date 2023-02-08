42730b078cd2fca19a359631306247429079df27
buzzind99 <buzzind99@gmail.com>
2023-02-08 14:00:11 +0700

    added AnimeFeatured component

---
+++ src/pages/anime/[id]/[title]/components/AnimeFeatured.tsx
@@ -0,0 +1,102 @@
+import Image from "next/image";
+import { IFeaturedData } from "@/types/interfaces";
+
+interface Props {
+  featuredData: IFeaturedData[];
+  id: number;
+  title: string;
+}
+
+const AnimeFeatured: React.FC<Props> = ({ featuredData, id, title }) => {
+  // Using div with onClick event listener on links with generic text
+  // (more, view more, etc.) for better SEO
+  const openInNewTab = (url: string) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
+
+  return (
+    <article
+      id="featured-articles"
+      className="mb-32 font-[Verdana] text-[0.75rem]"
+    >
+      <div
+        id="recent-anime-discussions_header"
+        className="mb-2 flex h-[1.375rem] items-center justify-between border-b-[1px] border-[#bebebe] pb-[0.1875rem] pt-1"
+      >
+        <h2 className="font-bold">
+          {
+            "Recent Featured Articles {this is a placeholder, links are external}"
+          }
+        </h2>
+        <div
+          onClick={() =>
+            openInNewTab(
+              `https://myanimelist.net/anime/${id}/${title}/featured`
+            )
+          }
+          className="float-right cursor-pointer pt-[0.125rem] pb-2 text-[0.6875rem] font-normal leading-tight text-[#1c439b] hover:underline"
+        >
+          More featured arcticles
+        </div>
+      </div>
+      <div id="featured-articles_content" className="flex-col">
+        {featuredData &&
+          featuredData.length &&
+          featuredData.map((data) => (
+            <div
+              id={`featured-articles_content-${data.id}`}
+              key={data.id}
+              className="mt-[0.375rem] flex border-b-[1px] border-[#e5e7eb] pb-[0.375rem]"
+            >
+              <a href={data.url} target="_blank" rel="noopener noreferrer">
+                <Image
+                  src={data.image_url}
+                  alt={data.title}
+                  width={50}
+                  height={70}
+                  className="h-[4.5rem] min-w-[3.25rem] border-[1px] border-[#bebebe] object-cover"
+                />
+              </a>
+              <div className="ml-2 inline-block text-[0.6875rem]">
+                <h3 className="pb-[0.1875rem] font-bold text-[#1c439b]">
+                  <a
+                    href={data.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title={data.title}
+                    className="text-[#1c439b] hover:underline"
+                  >
+                    {data.title}
+                  </a>
+                </h3>
+                <p>{data.description}</p>
+                <div className="mt-[0.375rem] flex items-center leading-relaxed text-[#808080]">
+                  <p>
+                    {`by `}
+                    <a
+                      href={data.author_url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="pr-2 text-[#1c439b] hover:underline"
+                    >
+                      {data.author}
+                    </a>
+                  </p>
+                  <p className="border-l-[1px] border-[#d8d8d8] pl-2 tracking-tighter">
+                    <b>{data.views.toLocaleString()}</b> views
+                  </p>
+                  {data.spoiler && (
+                    <p className="ml-2 border-l-[1px] border-[#d8d8d8] pl-2 font-bold tracking-tighter text-[#ff2c55]">
+                      Spoiler
+                    </p>
+                  )}
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </article>
+  );
+};
+
+export default AnimeFeatured;
