4c72f20f5d86af356b8ae9d5e79e86e3adcc6ebd
buzzind99 <buzzind99@gmail.com>
2023-02-08 21:33:49 +0700

    fixed type

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -107,11 +107,11 @@ export interface IAnimeData {
     url: string | null;
     embed_url: string | null;
     images: {
-      image_url: string;
-      small_image_url: string;
-      medium_image_url: string;
-      large_image_url: string;
-      maximum_image_url: string;
+      image_url: string | null;
+      small_image_url: string | null;
+      medium_image_url: string | null;
+      large_image_url: string | null;
+      maximum_image_url: string | null;
     };
   };
   approved: boolean;
