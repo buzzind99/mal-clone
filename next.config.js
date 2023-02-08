0c0913ad12549b84a9d577cd8f7bed09eec79752
buzzind99 <buzzind99@gmail.com>
2023-02-08 21:28:28 +0700

    added image protocol

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'cdn.myanimelist.net',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+      },
     ],
   },
 }
