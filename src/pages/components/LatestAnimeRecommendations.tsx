c8918d5c42da6c058a564080d0b7fe1642acccbb
buzzind99 <buzzind99@gmail.com>
2023-02-08 14:00:48 +0700

    refactored code

--- src/pages/components/LatestAnimeRecommendations.tsx
+++ src/pages/components/LatestAnimeRecommendations.tsx
@@ -1,22 +1,6 @@
 import Image from "next/image";
 import { IRecommendationsData } from "@/types/interfaces";
 
-const initialData: IRecommendationsData[] = [
-  {
-    id: 0,
-    anime: "",
-    anime_url: "",
-    image_url: "",
-    anime_rec: "",
-    anime_rec_url: "",
-    image_rec_url: "",
-    description: "",
-    user: "",
-    user_url: "",
-    time_since_posted: "",
-  },
-];
-
 interface Props {
   recommendationsData: IRecommendationsData[];
 }
@@ -24,8 +8,6 @@ interface Props {
 const LatestAnimeRecommendations: React.FC<Props> = ({
   recommendationsData,
 }) => {
-  const dummyData = recommendationsData || initialData;
-
   // Using div with onClick event listener on links with generic text
   // (more, view more, etc.) for better SEO
   const openInNewTab = (url: string) => {
@@ -63,109 +45,111 @@ const LatestAnimeRecommendations: React.FC<Props> = ({
         </div>
       </div>
       <div id="latest-anime-recommendations_content" className="flex-col"></div>
-      {dummyData.map((data) => (
-        <div
-          id={`latest-anime-recommendations_content-${data.id}`}
-          key={data.id}
-          className="mt-[0.375rem] border-b-[1px] border-[#e5e7eb] pb-[0.375rem] text-[0.6875rem]"
-        >
-          <div className="inline-block w-[50%]">
-            <div className="flex">
-              <a
-                href={data.anime_url}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  src={data.image_url}
-                  alt={data.anime}
-                  width={50}
-                  height={70}
-                  className="h-[4.5rem] min-w-[3.25rem] border-[1px] border-[#bebebe] object-cover"
-                />
-              </a>
-              <div className="ml-2">
-                <p className="mt-[0.2rem]">If you liked</p>
+      {recommendationsData &&
+        recommendationsData.length &&
+        recommendationsData.map((data) => (
+          <div
+            id={`latest-anime-recommendations_content-${data.id}`}
+            key={data.id}
+            className="mt-[0.375rem] border-b-[1px] border-[#e5e7eb] pb-[0.375rem] text-[0.6875rem]"
+          >
+            <div className="inline-block w-[50%]">
+              <div className="flex">
                 <a
                   href={data.anime_url}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="font-bold text-[#1c439b]"
                 >
-                  <h3 className="mt-1 w-fit cursor-pointer hover:underline">
-                    {data.anime}
-                  </h3>
+                  <Image
+                    src={data.image_url}
+                    alt={data.anime}
+                    width={50}
+                    height={70}
+                    className="h-[4.5rem] min-w-[3.25rem] border-[1px] border-[#bebebe] object-cover"
+                  />
                 </a>
-                <div className="mt-[0.375rem] h-[0.875rem] w-[1.5rem] cursor-pointer border-b-[1px] border-[#e5e5e5] bg-[#f0f0f0] px-[0.225rem] pt-[0.05rem] text-[0.625rem] tracking-tighter text-[#1c439b] transition-all duration-300 hover:text-white">
-                  add
+                <div className="ml-2">
+                  <p className="mt-[0.2rem]">If you liked</p>
+                  <a
+                    href={data.anime_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="font-bold text-[#1c439b]"
+                  >
+                    <h3 className="mt-1 w-fit cursor-pointer hover:underline">
+                      {data.anime}
+                    </h3>
+                  </a>
+                  <div className="mt-[0.375rem] h-[0.875rem] w-[1.5rem] cursor-pointer border-b-[1px] border-[#e5e5e5] bg-[#f0f0f0] px-[0.225rem] pt-[0.05rem] text-[0.625rem] tracking-tighter text-[#1c439b] transition-all duration-300 hover:text-white">
+                    add
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="inline-block w-[50%]">
-            <div className="flex">
-              <a
-                href={data.anime_rec_url}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  src={data.image_rec_url}
-                  alt={data.anime_rec}
-                  width={50}
-                  height={70}
-                  className="h-[4.5rem] min-w-[3.25rem] border-[1px] border-[#bebebe] object-cover"
-                />
-              </a>
-              <div className="ml-2">
-                <p className="mt-[0.2rem]">...then you might like</p>
+            <div className="inline-block w-[50%]">
+              <div className="flex">
                 <a
                   href={data.anime_rec_url}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="font-bold text-[#1c439b]"
                 >
-                  <h3 className="mt-1 w-fit cursor-pointer hover:underline">
-                    {data.anime_rec}
-                  </h3>
+                  <Image
+                    src={data.image_rec_url}
+                    alt={data.anime_rec}
+                    width={50}
+                    height={70}
+                    className="h-[4.5rem] min-w-[3.25rem] border-[1px] border-[#bebebe] object-cover"
+                  />
                 </a>
-                <div className="mt-[0.375rem] h-[0.875rem] w-[1.5rem] cursor-pointer border-b-[1px] border-[#e5e5e5] bg-[#f0f0f0] px-[0.225rem] pt-[0.05rem] text-[0.625rem] tracking-tighter text-[#1c439b] transition-all duration-300 hover:text-white">
-                  add
+                <div className="ml-2">
+                  <p className="mt-[0.2rem]">...then you might like</p>
+                  <a
+                    href={data.anime_rec_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="font-bold text-[#1c439b]"
+                  >
+                    <h3 className="mt-1 w-fit cursor-pointer hover:underline">
+                      {data.anime_rec}
+                    </h3>
+                  </a>
+                  <div className="mt-[0.375rem] h-[0.875rem] w-[1.5rem] cursor-pointer border-b-[1px] border-[#e5e5e5] bg-[#f0f0f0] px-[0.225rem] pt-[0.05rem] text-[0.625rem] tracking-tighter text-[#1c439b] transition-all duration-300 hover:text-white">
+                    add
+                  </div>
                 </div>
               </div>
             </div>
+            <div className="mt-[0.2rem]">
+              {`${data.description} `}
+              {readMore(data.description) && (
+                <div
+                  onClick={() =>
+                    openInNewTab(
+                      "https://myanimelist.net/recommendations.php?s=recentrecs&t=anime"
+                    )
+                  }
+                  className="inline-block cursor-pointer text-[#1c439b] hover:underline"
+                >
+                  read more
+                </div>
+              )}
+            </div>
+            <div className="mt-[0.225rem] flex items-center leading-relaxed text-[#808080]">
+              <p>
+                {`Anime rec by `}
+                <a
+                  href={data.user_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-[#1c439b] hover:underline"
+                >
+                  {data.user}
+                </a>
+                {` - ${data.time_since_posted}`}
+              </p>
+            </div>
           </div>
-          <div className="mt-[0.2rem]">
-            {`${data.description} `}
-            {readMore(data.description) && (
-              <div
-                onClick={() =>
-                  openInNewTab(
-                    "https://myanimelist.net/recommendations.php?s=recentrecs&t=anime"
-                  )
-                }
-                className="inline-block cursor-pointer text-[#1c439b] hover:underline"
-              >
-                read more
-              </div>
-            )}
-          </div>
-          <div className="mt-[0.225rem] flex items-center leading-relaxed text-[#808080]">
-            <p>
-              {`Anime rec by `}
-              <a
-                href={data.user_url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-[#1c439b] hover:underline"
-              >
-                {data.user}
-              </a>
-              {` - ${data.time_since_posted}`}
-            </p>
-          </div>
-        </div>
-      ))}
+        ))}
     </article>
   );
 };
