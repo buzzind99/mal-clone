3efd0882197006d400327a879ecfaee41f2c0dac
buzzind99 <buzzind99@gmail.com>
2023-02-09 19:39:59 +0700

    added components

--- src/pages/anime/[id]/[title]/index.tsx
+++ src/pages/anime/[id]/[title]/index.tsx
@@ -18,8 +18,17 @@ import AnimeTheme from "./components/AnimeTheme";
 import AnimeNews from "./components/AnimeNews";
 import AnimeMainStats from "./components/AnimeMainStats";
 import AnimeFeatured from "./components/AnimeFeatured";
+import AnimeNav from "./components/AnimeNav";
+import AnimeStacks from "./components/AnimeStacks";
+import AnimeNoData from "./components/AnimeNoData";
+import AnimeInfoNoData from "./components/AnimeInfoNoData";
 import { GetStaticPaths } from "next";
-import { IAnimeData, IFeaturedData, INewsData } from "@/types/interfaces";
+import {
+  IAnimeData,
+  IAnimeStacksData,
+  IFeaturedData,
+  INewsData,
+} from "@/types/interfaces";
 
 interface IParams {
   params: {
@@ -33,6 +42,7 @@ interface Props {
   animeData: IAnimeData;
   featuredData: IFeaturedData[];
   newsData: INewsData[];
+  stacksData: IAnimeStacksData[];
 }
 
 const Anime: React.FC<Props> = ({
@@ -40,6 +50,7 @@ const Anime: React.FC<Props> = ({
   animeData,
   newsData,
   featuredData,
+  stacksData,
 }) => {
   const desc_content = animeData.synopsis
     ? animeData.synopsis.length > 347
@@ -84,34 +95,19 @@ const Anime: React.FC<Props> = ({
             <AnimeTitles titles={animeData.titles} />
             <AnimeInfo animeData={animeData} />
             <AnimeStats animeData={animeData} />
-            <div id="available-at" className="mb-5">
-              <h2
-                id="available-at_header"
-                className="mb-[0.45rem] border-b-[1px] border-[#bebebe] py-[0.15rem] text-[0.75rem] font-bold "
-              >
-                Available At
-              </h2>
-              <span className="text-[0.6875rem] font-bold text-[#444]">
-                No Data
-              </span>
-            </div>
-            <div id="resources" className="mb-5">
-              <h2
-                id="resources_header"
-                className="mb-[0.45rem] border-b-[1px] border-[#bebebe] py-[0.15rem] text-[0.75rem] font-bold "
-              >
-                Resources
-              </h2>
-              <span className="text-[0.6875rem] font-bold text-[#444]">
-                No Data
-              </span>
-            </div>
+            <AnimeInfoNoData id_text="available-at" title="Available At" />
+            <AnimeInfoNoData id_text="resources" title="Resources" />
             <AnimeStreaming streaming={animeData.streaming} />
           </div>
           <div
             id="content-right"
             className="inline-block w-[51.125rem] pl-[0.3rem] pr-[0.625rem]"
           >
+            <AnimeNav
+              id={animeData.mal_id}
+              title={animeData.title}
+              title_url={title}
+            />
             <AnimeMainStats animeData={animeData} />
             <AnimeSynopsis synopsis={animeData.synopsis} />
             <AnimeBackground background={animeData.background} />
@@ -119,11 +115,22 @@ const Anime: React.FC<Props> = ({
               <MALxJPN />
             </div>
             <AnimeRelations relations={animeData.relations} />
+            <AnimeNoData title="Staff" link_title="More staff" />
             <AnimeTheme theme={animeData.theme} />
+            <AnimeNoData title="Reviews" link_title="More reviews" />
+            <AnimeStacks
+              stacks={stacksData}
+              id={animeData.mal_id}
+              title={title}
+            />
             <AnimeNews
               newsData={newsData}
               id={animeData.mal_id}
               title={title}
+            />
+            <AnimeNoData
+              title="Recent Forum Discussion"
+              link_title="More discussions"
             />
             <AnimeFeatured
               featuredData={featuredData}
@@ -174,9 +181,12 @@ export const getStaticProps = async (context: IParams) => {
   const featuredData = await import(
     "@/pages/components/dummy_data/featuredData.json"
   ).then((res) => res.default.data.slice(0, 2));
+  const stacksData = await import(
+    "./components/dummy_data/stacksData.json"
+  ).then((res) => res.default.data);
 
   return {
-    props: { title, animeData, newsData, featuredData },
+    props: { title, animeData, newsData, featuredData, stacksData },
   };
 };
 
