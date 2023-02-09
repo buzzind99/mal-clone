0ef9e5f16c5ef9fb0198cdf515d3163c4cbc99b6
buzzind99 <buzzind99@gmail.com>
2023-02-09 19:38:48 +0700

    redirects /anime to /under_construction

--- src/pages/anime/index.tsx
+++ src/pages/anime/index.tsx
@@ -1,30 +1,12 @@
-import ContentTitleBar from "@/components/ContentTitleBar";
-import FooterChartPlaceholder from "@/components/FooterChartPlaceholder";
-import MainContainer from "@/components/MainContainer";
-import MainFooter from "@/components/MainFooter";
-import MainHeader from "@/components/MainHeader";
-import NavBar from "@/components/NavBar";
-import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>MyAnimeList.net - Anime and Manga Database and Community</title>
-        <meta
-          name="description"
-          content="Welcome to MyAnimeList, the world&#039;s most active online anime and manga community and database. Join the online community, create your anime and manga list, read reviews, explore the forums, follow news, and so much more!"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="keywords" content="anime, myanimelist, anime news, manga" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <MainHeader />
-      <NavBar />
-      <ContentTitleBar title="Anime" />
-      <MainContainer />
-      <FooterChartPlaceholder />
-      <MainFooter />
-    </>
-  );
-}
+const Anime = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace(`/under_construction`);
+  }, []);
+};
+
+export default Anime;
