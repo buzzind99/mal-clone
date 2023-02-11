ccbb5935cc7335b80663a3921739fe2018187257
buzzind99 <buzzind99@gmail.com>
2023-02-11 23:58:22 +0700

    changed variable name

--- src/pages/anime/[id]/[title]/components/AnimeStacks.tsx
+++ src/pages/anime/[id]/[title]/components/AnimeStacks.tsx
@@ -4,10 +4,10 @@ import { IAnimeStacksData } from "@/types/interfaces";
 interface Props {
   stacks: IAnimeStacksData[];
   id: number;
-  title: string;
+  title_url: string;
 }
 
-const AnimeStacks: React.FC<Props> = ({ stacks, id, title }) => {
+const AnimeStacks: React.FC<Props> = ({ stacks, id, title_url }) => {
   // Using div with onClick event listener on links with generic text
   // (more, view more, etc.) for better SEO
   const openInNewTab = (url: string) => {
@@ -26,7 +26,9 @@ const AnimeStacks: React.FC<Props> = ({ stacks, id, title }) => {
         </span>
         <span
           onClick={() =>
-            openInNewTab(`https://myanimelist.net/anime/${id}/${title}/stacks`)
+            openInNewTab(
+              `https://myanimelist.net/anime/${id}/${title_url}/stacks`
+            )
           }
           className="cursor-pointer text-[0.6875rem] font-normal text-[#1c439b] hover:underline"
         >
@@ -43,13 +45,18 @@ const AnimeStacks: React.FC<Props> = ({ stacks, id, title }) => {
                 index === stacks.length - 1 ? "" : "mr-[0.625rem]"
               }`}
             >
-              <div className="relative inline-block h-[7.75rem] w-[6.875rem]">
+              <a
+                href={stack.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="relative inline-block h-[7.75rem] w-[6.875rem]"
+              >
                 <Image
                   src={stack.image_2}
                   alt={stack.title}
                   width={85}
                   height={124}
-                  className="z-2 absolute left-[1rem] top-0 h-[7.75rem] w-[85px] object-cover"
+                  className="z-2 absolute left-[1rem] top-0 h-[7.75rem] w-[5.3125rem] object-cover"
                   style={{ boxShadow: "2px 2px 5px 0 rgb(0 0 0 / 40%)" }}
                 />
                 <Image
@@ -57,10 +64,10 @@ const AnimeStacks: React.FC<Props> = ({ stacks, id, title }) => {
                   alt={stack.title}
                   width={85}
                   height={124}
-                  className="z-1 absolute left-0 top-0 h-[7.75rem] w-[85px] object-cover"
+                  className="z-1 absolute left-0 top-0 h-[7.75rem] w-[5.3125rem] object-cover"
                   style={{ boxShadow: "2px 2px 5px 0 rgb(0 0 0 / 40%)" }}
                 />
-              </div>
+              </a>
               <div className="inline-block w-[9.375rem] align-top text-[0.6875rem]">
                 <div className="my-2 font-bold text-[#1c439b]">
                   <a
